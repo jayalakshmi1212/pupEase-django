e94e0b151bd287c23bc3f5a6302cd6e93f289782
Jayalakshmi.M <jayalakshmim720@gmail.com>
2024-06-14 14:18:56 +0530

    images problem solved

--- ecom_p/settings.py
+++ ecom_p/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticfiles')]
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 # Default primary key field type
--- ecom_p/urls.py
+++ ecom_p/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path("",include('order.urls')),
 ]
 
-if settings.DEBUG:
-   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
