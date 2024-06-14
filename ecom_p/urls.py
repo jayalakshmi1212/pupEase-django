62f0d52bd47c6ab617da237d54bdbbd75bf54780
Jayalakshmi.M <jayalakshmim720@gmail.com>
2024-06-14 14:28:33 +0530

    debug true in aws also

--- ecom_p/urls.py
+++ ecom_p/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path("",include('order.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
